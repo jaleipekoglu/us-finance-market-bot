6429c36ad7135e7f2671cb1c704c98bf50782f09
jaleipekoglu <47296000+jaleipekoglu@users.noreply.github.com>
2026-02-09 16:08:54 +0300

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,1 +1,15 @@
+import feedparser
+
+def fetch_news(sources):
+    items = []
+    for s in sources:
+        feed = feedparser.parse(s["rss"])
+        for e in feed.entries:
+            items.append({
+                "source": s["name"],
+                "title": e.title,
+                "summary": e.get("summary", ""),
+                "link": e.link
+            })
+    return items
 
