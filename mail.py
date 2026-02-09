8e27a596b635efb02ce3df05c3648779a627da72
jaleipekoglu <47296000+jaleipekoglu@users.noreply.github.com>
2026-02-09 16:10:14 +0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,1 +1,18 @@
+import smtplib
+from email.message import EmailMessage
+import os
+
+def send_mail(content):
+    msg = EmailMessage()
+    msg["Subject"] = "US Finance Daily Highlights"
+    msg["From"] = os.environ["MAIL_USER"]
+    msg["To"] = os.environ["MAIL_USER"]
+    msg.set_content(content)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(
+            os.environ["MAIL_USER"],
+            os.environ["MAIL_PASS"]
+        )
+        smtp.send_message(msg)
 
