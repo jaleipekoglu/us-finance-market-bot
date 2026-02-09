51c2d3cad9695879aab5766c7fb50604877a2ed7
jaleipekoglu <47296000+jaleipekoglu@users.noreply.github.com>
2026-02-09 16:08:34 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,1 +1,15 @@
+SOURCES = [
+    {
+        "name": "Axios AM",
+        "rss": "https://www.axios.com/feeds/newsletters/axios-am"
+    },
+    {
+        "name": "Reuters Finance",
+        "rss": "https://www.reuters.com/finance/rss"
+    },
+    {
+        "name": "Crunchbase News",
+        "rss": "https://news.crunchbase.com/feed/"
+    }
+]
 
