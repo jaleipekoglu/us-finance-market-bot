50b9717a402da5f37935324777088da912c591d0
jaleipekoglu <47296000+jaleipekoglu@users.noreply.github.com>
2026-02-09 16:10:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from sources import SOURCES
+from crawl import fetch_news
+from filter import filter_news
+from summarize import summarize
+from mail import send_mail
 
+items = fetch_news(SOURCES)
+filtered = filter_news(items)
+
+if filtered:
+    send_mail(summarize(filtered))
